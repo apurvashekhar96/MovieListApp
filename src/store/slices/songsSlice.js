eeedbc4dabf3d5537aa84225a9f458bc17557489
Apurva Shekhar <103213540+apurvashekhar96@users.noreply.github.com>
2023-01-15 13:46:51 +0530

    initial commit

---
+++ src/store/slices/songsSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+import { reset } from "../actions";
+
+const songsSlice = createSlice({
+  name: "song",
+  initialState: [],
+  reducers: {
+    addSong(state, action) {
+      state.push(action.payload);
+    },
+    removeSong(state, action) {
+      const songToRemove = state.indexOf(action.payload);
+      state.splice(songToRemove, 1);
+    },
+  },
+  extraReducers(builder) {
+    builder.addCase(reset, (state, action) => {
+      return [];
+    });
+  },
+});
+
+export const { addSong, removeSong } = songsSlice.actions;
+export const songsReducer = songsSlice.reducer;
